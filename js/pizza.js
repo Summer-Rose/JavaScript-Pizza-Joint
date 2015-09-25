1668bd71fa9f2b8cf15e809fcc3fa352bde4339f
Summer <sbrochtrup@gmail.com>
2015-09-24 22:54:43 -0700

    Add getPrice prototype function

--- js/pizza.js
+++ js/pizza.js
@@ -8,3 +8,29 @@ Pizza.prototype.addTopping = function(topping) {
 	this.toppings.push(topping);
 } 
 
+Pizza.prototype.getPrice = function() {
+	var meats = ["pepperoni", "sausage", "bacon"];
+	var veggies = ["black olives", "artichoke hearts", "bell peppers"];
+	var total = 0;
+	if (this.size == "Large") {
+		total += 20;
+	} else if (this.size == "Medium") {
+		total += 15;
+	} else {
+		total += 10;
+	}
+
+	for (var i = 0; i < this.toppings.length; i++) {
+		for (var j = 0; j < meats.length; j++) {
+			if (this.toppings[i] == meats[j]) {
+				total += .75;
+			}
+		}
+		for (var j = 0; j < veggies.length; j++) {
+			if (this.toppings[i] == veggies[j]) {
+				total += .50;
+			}
+		}
+	}	
+	return total;
+}
