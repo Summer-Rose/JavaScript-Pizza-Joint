61d804f6c2ba37cbe39c6acd8b6bd961d16455f9
Summer <sbrochtrup@gmail.com>
2015-09-24 22:34:14 -0700

    Add specs to test Pizza constructor and addToppings function

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
-describe('functionName', function() {
-  it("describe behavior", function() {
-    expect(functionName(parameter)).to.equal(result);
+describe('Pizza', function() {
+  it("Gets correct size of new instance of Pizza", function() {
+  	var newPizza = new Pizza("Large");
+    expect(newPizza.size).to.equal("Large");
+  });
+
+  it("Returns correct topping array of new instance of Pizza", function() {
+  	var newPizza = new Pizza("Large");
+  	newPizza.addTopping("Mushrooms");
+  	newPizza.addTopping("Sun Dried Tomatoes");
+    expect(newPizza.toppings).to.eql(["Mushrooms", "Sun Dried Tomatoes"]);
   });
 });
